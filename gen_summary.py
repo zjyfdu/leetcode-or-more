90644ba74ca29f2ef526893420c8a590b32aec49
Zhai Jinyuan <15210720098@fudan.edu.cn>
2021-08-07 15:30:17 +0800

    gen_summary.py

--- gen_summary.py
+++ gen_summary.py
@@ -13,4 +13,4 @@ for root, dirs, files in os.walk("."):
         print("* [{}]()".format(root[2:]))
         for f in files:
             if f.endswith('.md'):
-                print("\t* [{0}]({1})".format(f[:-3], f))
+                print("\t* [{0}]({1})".format(f[:-3], os.path.join(root[2:], f)))
